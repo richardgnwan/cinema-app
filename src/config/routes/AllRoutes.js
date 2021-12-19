5ad8d5a4ffbd978569c82ed6623cb9e6ad3e8438
richardgun26 <richardgunawan26@gmail.com>
2021-12-19 22:13:56 +0700

    tampilan rg

--- src/config/routes/AllRoutes.js
+++ src/config/routes/AllRoutes.js
@@ -7,6 +7,7 @@ import MainHomePage from '../../pages/main/MainHomePage';
 import MainTicketsPage from '../../pages/main/MainTicketsPage';
 import MainTicketsQrPage from '../../pages/main/MainTicketsQrPage';
 import MainTicketsInvoicePage from '../../pages/main/MainTicketsInvoicePage';
+import MainTopupPage from '../../pages/main/MainTopupPage';
 
 // ICONS
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
@@ -14,8 +15,9 @@ import AccountBoxIcon from '@mui/icons-material/AccountBox';
 export const MAIN_ROUTES = [
   { label: 'Home', path: '/', icon: <AccountBoxIcon />, page: <MainHomePage /> },
   { label: 'Tickets', path: '/tickets', icon: <AccountBoxIcon />, page: <MainTicketsPage /> },
-  { label: 'Tickets QR', path: '/tickets/qr', icon: <AccountBoxIcon />, page: <MainTicketsQrPage /> },
-  // { label: 'Tickets Invoice', path: '/tickets/invoice', icon: <AccountBoxIcon />, page: <MainTicketsInvoicePage /> },
+  { label: 'Tickets QR', path: '/tickets/:id_ticket/qr', icon: <AccountBoxIcon />, page: <MainTicketsQrPage /> },
+  { label: 'Tickets Invoice', path: '/tickets/:id_ticket/invoice', icon: <AccountBoxIcon />, page: <MainTicketsInvoicePage /> },
+  { label: 'Topup', path: '/topup', icon: <AccountBoxIcon />, page: <MainTopupPage /> },
 ]
 
 export const ADMIN_ROUTES = [
--- src/pages/main/MainTicketsInvoicePage.jsx
+++ src/pages/main/MainTicketsInvoicePage.jsx
@@ -1,10 +1,77 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import './css/invoice.css';
 
 const MainTicketsInvoicePage = () => {
+
   return (
-    <div>
-      
-    </div>
+    <Fragment>
+      <button onClick={() => window.print()}>PRINT</button>
+      <div className="invoice-box">
+        <table cellPadding={0} cellSpacing={0}>
+          <tbody><tr className="top">
+            <td colSpan={2}>
+              <table>
+                <tbody><tr>
+                  <td className="title">
+                    <img src="https://www.sparksuite.com/images/logo.png" style={{ width: '100%', maxWidth: '300px' }} />
+                  </td>
+                  <td>
+                    Invoice #: 123<br />
+                    Created: January 1, 2015<br />
+                    Due: February 1, 2015
+                  </td>
+                </tr>
+                </tbody></table>
+            </td>
+          </tr>
+            <tr className="information">
+              <td colSpan={2}>
+                <table>
+                  <tbody><tr>
+                    <td>
+                      Cinema App, Inc.<br />
+                    </td>
+                    <td>
+                      Acme Corp.<br />
+                      Admin<br />
+                      admin@example.com
+                    </td>
+                  </tr>
+                  </tbody></table>
+              </td>
+            </tr>
+            <tr className="heading">
+              <td>Payment Method</td>
+              <td>Check #</td>
+            </tr>
+            <tr className="">
+              <td>Check</td>
+              <td>1000</td>
+            </tr>
+            <tr className="heading">
+              <td>Item</td>
+              <td>Price</td>
+            </tr>
+            <tr className="item">
+              <td>Website design</td>
+              <td>$300.00</td>
+            </tr>
+            <tr className="item">
+              <td>Hosting (3 months)</td>
+              <td>$75.00</td>
+            </tr>
+            <tr className="item last">
+              <td>Domain name (1 year)</td>
+              <td>$10.00</td>
+            </tr>
+            <tr className="total">
+              <td />
+              <td>Total: $385.00</td>
+            </tr>
+          </tbody></table>
+      </div>
+    </Fragment>
+
   )
 }
 
