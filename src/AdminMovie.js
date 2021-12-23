59bba6e42c8c936032c92057363a78ed9e6b3f8b
monicachandra <56423441+monicachandra@users.noreply.github.com>
2021-12-23 22:47:21 +0700

    movie sm voucher, no backend uda

--- src/AdminMovie.js
+++ src/AdminMovie.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import { query, where } from "firebase/firestore";
 import {db} from './config/auth/firebase'
 import {
     collection,
@@ -9,18 +10,33 @@ import {
     deleteDoc,
     doc,
 } from "firebase/firestore";
+import Modal from '@material-ui/core/Modal';
+import React from 'react';
 
 function AdminMovie() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
     const [newTitle, setNewTitle] = useState("");
     const [newSynopsis, setNewSynopsis] = useState("");
     const [newPoster, setNewPoster] = useState("");
     const [newDuration, setNewDuration] = useState(0);
 
+    const [updateId, setUpdateId] = useState("");
+    const [updateTitle, setUpdateTitle] = useState("");
+    const [updateSynopsis, setUpdateSynopsis] = useState("");
+    const [updatePoster, setUpdatePoster] = useState("");
+    const [updateDuration, setUpdateDuration] = useState(0);
+
     const [movies, setMovies] = useState([]);
     const usersCollectionRef = collection(db, "movie");
 
     const getMovies = async () => {
-        const data = await getDocs(usersCollectionRef);
+        const movieRef = collection(db, "movie");
+        const q = query(movieRef, where("isActive", "==", 1),where("isDeleted", "==", 0));
+        const data = await getDocs(q);
         setMovies(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
 
@@ -28,6 +44,10 @@ function AdminMovie() {
         await addDoc(usersCollectionRef, { title: newTitle, synopsis : newSynopsis,
                                                 poster : newPoster, duration: Number(newDuration),
                                                 isActive:Number(1),isDeleted:Number(0) });
+        setNewTitle('');
+        setNewSynopsis('');
+        setNewPoster('');
+        setNewDuration(0);
         getMovies();
     };
 
@@ -42,16 +62,31 @@ function AdminMovie() {
         await updateDoc(movieDoc,newFields);
     }
 
-    const updateUser = async (id, age) => {
-        const userDoc = doc(db, "users", id);
-        const newFields = { age: age + 1 };
+    const updateMovie = async () => {
+        const userDoc = doc(db, "movie", updateId);
+        const newFields = { title: updateTitle,synopsis:updateSynopsis,poster:updatePoster,duration:updateDuration };
         await updateDoc(userDoc, newFields);
+        setUpdateId('');
+        setUpdateTitle('');
+        setUpdateSynopsis('');
+        setUpdatePoster('');
+        setUpdateDuration('');
+        handleClose();
         getMovies();
     };
 
     useEffect(() => {
         getMovies();
     }, []);
+
+    const handleOpen = async (id,title,synopsis,poster,duration) => {
+        setUpdateId(id);
+        setUpdateTitle(title);
+        setUpdateSynopsis(synopsis);
+        setUpdatePoster(poster);
+        setUpdateDuration(duration);
+        setOpen(true);
+    };
 
     return (
         <div className="App">
@@ -60,18 +95,21 @@ function AdminMovie() {
                 onChange={(event) => {
                     setNewTitle(event.target.value);
                 }}
+                value={newTitle}
             />
             <input
                 placeholder="Sinopsis"
                 onChange={(event) => {
                     setNewSynopsis(event.target.value);
                 }}
+                value={newSynopsis}
             />
             <input
                 placeholder="Link Foto Poster"
                 onChange={(event) => {
                     setNewPoster(event.target.value);
                 }}
+                value={newPoster}
             />
             <input
                 type="number"
@@ -79,6 +117,7 @@ function AdminMovie() {
                 onChange={(event) => {
                     setNewDuration(event.target.value);
                 }}
+                value={newDuration}
             />
 
             <button onClick={addMovie}> Add Movie</button>
@@ -103,9 +142,63 @@ function AdminMovie() {
                         >
                             Hapus
                         </button>
+                        <button type="button"
+                                onClick={() => {
+                                    handleOpen(movie.id,movie.title,movie.synopsis,movie.poster,movie.duration);
+                                }}
+                        >
+                            Update Movie
+                        </button>
                     </div>
                 );
             })}
+            <Modal
+                onClose={handleClose}
+                open={open}
+                style={{
+                    position: 'absolute',
+                    border: '2px solid #000',
+                    backgroundColor: 'gray',
+                    boxShadow: '2px solid black',
+                    height:500,
+                    width: 240,
+                    margin: 'auto'
+                }}
+            >
+                <div>
+                    <div>
+                        <input
+                            placeholder="Title"
+                            onChange={(event) => {
+                                setUpdateTitle(event.target.value);
+                            }}
+                            value={updateTitle}
+                        />
+                        <input
+                            placeholder="Synopsis"
+                            onChange={(event) => {
+                                setUpdateSynopsis(event.target.value);
+                            }}
+                            value={updateSynopsis}
+                        />
+                        <input
+                            placeholder="Poster"
+                            onChange={(event) => {
+                                setUpdatePoster(event.target.value);
+                            }}
+                            value={updatePoster}
+                        />
+                        <input
+                            placeholder="Duration"
+                            onChange={(event) => {
+                                setUpdateDuration(event.target.value);
+                            }}
+                            value={updateDuration}
+                        />
+                        <button onClick={updateMovie}>Update</button>
+                    </div>
+                </div>
+            </Modal>
         </div>
     );
 }
--- src/AdminVoucher.js
+++ src/AdminVoucher.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
+import React from 'react';
 import "./App.css";
 import {db} from './config/auth/firebase'
+import { query, where } from "firebase/firestore";
+import Modal from '@material-ui/core/Modal';
 import {
     collection,
     getDocs,
@@ -11,20 +14,36 @@ import {
 } from "firebase/firestore";
 
 function AdminVoucher() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+
+
     const [newKodeUnik, setNewKodeUnik] = useState("");
     const [newSaldo, setNewSaldo] = useState(0);
 
     const [voucher, setVoucher] = useState([]);
     const voucherCollectionRef = collection(db, "voucher");
 
+    const [updateKodeUnik, setUpdateKodeUnik] = useState("");
+    const [updateSaldo, setUpdateSaldo] = useState(0);
+    const [updateId, setUpdateId] = useState("");
+
     const getVoucher = async () => {
-        const data = await getDocs(voucherCollectionRef);
+        const voucherRef = collection(db, "voucher");
+        const q = query(voucherRef, where("isDeleted", "==", 0),where("isUsed", "==", 0));
+        const data = await getDocs(q);
         setVoucher(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
 
     const addVoucher = async () => {
         await addDoc(voucherCollectionRef, { kodeUnik: newKodeUnik, saldo : Number(newSaldo),
             isUsed:Number(0),isDeleted:Number(0) });
+        setNewKodeUnik('');
+        setNewSaldo('');
         getVoucher();
     };
 
@@ -35,16 +54,29 @@ function AdminVoucher() {
         getVoucher();
     }
 
-    const updateUser = async (id, age) => {
-        const userDoc = doc(db, "users", id);
-        const newFields = { age: age + 1 };
+    const handleOpen = async (id,kodeUnik,saldo) => {
+        setUpdateId(id);
+        setUpdateKodeUnik(kodeUnik);
+        setUpdateSaldo(saldo);
+        setOpen(true);
+    };
+
+    const updateVoucher = async () => {
+        const userDoc = doc(db, "voucher", updateId);
+        const newFields = { kodeUnik: updateKodeUnik, saldo:updateSaldo};
         await updateDoc(userDoc, newFields);
+        setUpdateId('');
+        setUpdateKodeUnik('');
+        setUpdateSaldo('');
+        handleClose();
         getVoucher();
     };
 
     useEffect(() => {
         getVoucher();
     }, []);
+
+
 
     return (
         <div className="App">
@@ -53,6 +85,7 @@ function AdminVoucher() {
                 onChange={(event) => {
                     setNewKodeUnik(event.target.value);
                 }}
+                value={newKodeUnik}
             />
             <input
                 type="number"
@@ -60,6 +93,7 @@ function AdminVoucher() {
                 onChange={(event) => {
                     setNewSaldo(event.target.value);
                 }}
+                value={newSaldo}
             />
 
             <button onClick={addVoucher}> Add Voucher</button>
@@ -75,9 +109,48 @@ function AdminVoucher() {
                         >
                             Hapus
                         </button>
+                        <button type="button"
+                            onClick={() => {
+                                handleOpen(voucher.id,voucher.kodeUnik,voucher.saldo);
+                            }}
+                        >
+                            Update Voucher
+                        </button>
                     </div>
                 );
             })}
+            <Modal
+                onClose={handleClose}
+                open={open}
+                style={{
+                    position: 'absolute',
+                    border: '2px solid #000',
+                    backgroundColor: 'gray',
+                    boxShadow: '2px solid black',
+                    height:80,
+                    width: 240,
+                    margin: 'auto'
+                }}
+            >
+                <div>
+                    <input
+                        placeholder="Kode Unik"
+                        onChange={(event) => {
+                            setUpdateKodeUnik(event.target.value);
+                        }}
+                        value={updateKodeUnik}
+                    />
+                    <input
+                        type="number"
+                        placeholder="Saldo"
+                        onChange={(event) => {
+                            setUpdateSaldo(event.target.value);
+                        }}
+                        value={updateSaldo}
+                    />
+                    <button onClick={updateVoucher}>Update</button>
+                </div>
+            </Modal>
         </div>
     );
 }
