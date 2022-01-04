d657e382ce677caeea7329e94e4ad5cedb9e4ee9
Jessica Nathania <jessicanathaniawidjaja@gmail.com>
2022-01-04 23:41:36 +0700

    error detai ljam

--- src/pages/main/MoviesDetailPage.jsx
+++ src/pages/main/MoviesDetailPage.jsx
@@ -189,20 +189,18 @@ export default function MoviesDetailPage() {
 
             </div>
             <div className="card__wrap">
-                <ul className="card__list">
-                    {jadwals && jadwals.map((j) => {
-                        return(
-                            <Link to={'/pilih-kursi/'+ j.id}>
-                                {/*<div key={j.id}>*/}
-                                    <div className="price">
-                                        <li className="card__cover" style={{width:150}}>{j.tanggal} {j.jamAwal}</li>
-                                    </div>
-                                {/*</div>*/}
-                            </Link>
+                {/*<ul className="card__list">*/}
+                <Link to='/tickets'>asdf</Link>
+                {jadwals && jadwals.map((j) => {
+                    return(
+                        <div  key={j.id}>
+                            <button className="card__cover" style={{width:150}}>{j.tanggal} {j.jamAwal}</button>
+                        </div>
 
-                        )
-                    })}
-                </ul>
+
+                    )
+                })}
+                {/*</ul>*/}
             </div>
 
             </div>
